2fd79c044c9b7fc591cfbe05957c4f440d6e8825
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-21 10:47:52 +0200

    setup classes added

---
+++ smite-cli/Setup.fs
@@ -0,0 +1,17 @@
+﻿namespace TIKSN.smite.cli
+
+open TIKSN.Configuration
+open TIKSN.DependencyInjection
+open Autofac
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+
+type ConfigurationRootSetup() =
+    inherit ConfigurationRootSetupBase()
+
+type CompositionRootSetup(configurationRoot) =
+    inherit AutofacPlatformCompositionRootSetupBase(configurationRoot)
+    override this.ConfigureContainerBuilder(builder: ContainerBuilder) =
+        ()
+    override this.ConfigureOptions(services: IServiceCollection, configuration: IConfigurationRoot) =
+        ()
