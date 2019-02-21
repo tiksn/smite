4ccb8c766c60bfd036980fbc3ebc2bf2cd312e18
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-21 11:07:48 +0200

    cli arguments added

--- smite-cli/Program.fs
+++ smite-cli/Program.fs
@@ -1,8 +1,32 @@
-﻿// Learn more about F# at http://fsharp.org
-
+﻿
 open System
+open Argu
+open TIKSN.smite.cli
+
+type SupportedProgrammingLanguage =
+    | FSharp = 1
+    | CSharp = 2
+    | TypeScript = 3
+
+type CLIArguments =
+    | Lang of SupportedProgrammingLanguage
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Lang _ -> "Supported Programming Language"
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let configurationRootSetup = new ConfigurationRootSetup()
+    let configurationRoot = configurationRootSetup.GetConfigurationRoot();
+    let compositionRootSetup = new CompositionRootSetup(configurationRoot)
+    let serviceProvider = compositionRootSetup.CreateServiceProvider()
+    let parser = ArgumentParser.Create<CLIArguments>(programName = "smite-cli.dll")
+    try
+        let results =  parser.ParseCommandLine(inputs = argv, raiseOnUsage = true)
+        let all = results.GetAllResults()
+        0
+    with e ->
+        printfn "%s" e.Message
+        1
