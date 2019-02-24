ace1588113e7707e5d4047a9ed44498e883fba3c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-25 00:25:46 +0200

    enabled visual basic

--- smite-lib/VisualBasicTranspiler.fs
+++ smite-lib/VisualBasicTranspiler.fs
@@ -1,11 +1,13 @@
 ﻿namespace TIKSN.smite.lib
 
 module VisualBasicTranspiler =
+    open Microsoft.CodeAnalysis.Editing
+    open Microsoft.CodeAnalysis
+
     let fileExtension = ".vb"
 
-    let getTypeName(t: FieldType) =
-        match t with
-        | FieldType.BooleanType -> "Boolean"
-        | FieldType.IntegerType -> "Integer"
-        | FieldType.RealType -> "Double"
-        | FieldType.StringType -> "String"
+    let transpile(models: seq<NamespaceDefinition>) =
+        let syntaxGenerator = SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.VisualBasic)
+        let filespaceDefinitions = CommonFeatures.getFilespaceDefinitions(models)
+        filespaceDefinitions
+        |> Seq.collect (fun x -> RoslynTranspiler.transpileFilespaceDefinition(syntaxGenerator, fileExtension, x))
