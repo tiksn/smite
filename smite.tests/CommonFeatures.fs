8ddfc0f32868cfc5c372f3750bacb742f5937e8c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-24 06:28:44 +0200

    another test added

--- smite.tests/CommonFeatures.fs
+++ smite.tests/CommonFeatures.fs
@@ -10,7 +10,7 @@ let tests =
     testCase "no common segment" <| fun _ ->
       let modelsArray = [||]
       let nsd = seq [{Namespace=[|"Microsoft"; "Graph"|]; Models=modelsArray} ;
-                    {Namespace=[|"System"; "Collection"; "Generic"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"; "Generic"|]; Models=modelsArray} ;
                     {Namespace=[|"TIKSN"; "smite"; "lib"; "core"|]; Models=modelsArray}]
       let subject = CommonFeatures.getFilespaceDefinition(nsd)
       Expect.equal (nsd |> Seq.length) (subject |> Seq.length) "Count must be equal"
@@ -23,6 +23,22 @@ let tests =
         for j = 0 to nsArray.[i].Length - 1 do
             if i <> j then
                 Expect.equal nsArray.[i].[j] fsArray.[i].[j] "Item values must be equal"
+
+    testCase "One common segment" <| fun _ ->
+      let modelsArray = [||]
+      let nsd = seq [{Namespace=[|"System"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"; "Generic"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"; "Specialized "|]; Models=modelsArray}]
+      let subject = CommonFeatures.getFilespaceDefinition(nsd)
+      Expect.equal (nsd |> Seq.length) (subject |> Seq.length) "Count must be equal"
+
+      let nsArray = nsd |> Seq.map (fun x -> x.Namespace) |> Seq.toArray
+      let fsArray = subject |> Seq.map (fun x -> x.Filespace) |> Seq.toArray
+
+      let itemCount = nsArray.Length
+      for i = 0 to itemCount - 1 do
+        for j = 0 to nsArray.[i].Length - 2 do
+            if i <> j then
+                Expect.equal nsArray.[i].[j + 1] fsArray.[i].[j] "Item values must be equal"
   ]
-
-
