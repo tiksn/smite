773dc3e5ec10d45a3338e6153d6229238cbcfc3c
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-02-24 06:30:00 +0200

    one more test added

--- smite.tests/CommonFeatures.fs
+++ smite.tests/CommonFeatures.fs
@@ -41,4 +41,21 @@ let tests =
         for j = 0 to nsArray.[i].Length - 2 do
             if i <> j then
                 Expect.equal nsArray.[i].[j + 1] fsArray.[i].[j] "Item values must be equal"
+
+    testCase "Two common segment" <| fun _ ->
+      let modelsArray = [||]
+      let nsd = seq [{Namespace=[|"System"; "Collections"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"; "Generic"|]; Models=modelsArray} ;
+                    {Namespace=[|"System"; "Collections"; "Specialized "|]; Models=modelsArray}]
+      let subject = CommonFeatures.getFilespaceDefinition(nsd)
+      Expect.equal (nsd |> Seq.length) (subject |> Seq.length) "Count must be equal"
+
+      let nsArray = nsd |> Seq.map (fun x -> x.Namespace) |> Seq.toArray
+      let fsArray = subject |> Seq.map (fun x -> x.Filespace) |> Seq.toArray
+
+      let itemCount = nsArray.Length
+      for i = 0 to itemCount - 1 do
+        for j = 0 to nsArray.[i].Length - 3 do
+            if i <> j then
+                Expect.equal nsArray.[i].[j + 2] fsArray.[i].[j] "Item values must be equal"
   ]
