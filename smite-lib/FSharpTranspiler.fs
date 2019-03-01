6211d02cdfe866a264bf5a96eef27ca797d4a501
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-02 00:58:39 +0200

    generate namespaces, usings and modules

--- smite-lib/FSharpTranspiler.fs
+++ smite-lib/FSharpTranspiler.fs
@@ -2,6 +2,47 @@ namespace TIKSN.smite.lib
 
 module FSharpTranspiler =
     open FSharp.Compiler.Ast
+    open IndentationFeatures
 
     let fileExtension = ".fs"
-    let transpile (models : seq<NamespaceDefinition>) = []
+    let indentSpaces = 4
+
+    let generateSourceFileCode (ns : string [], moduleName : string,
+                                models : ModelDefinition []) =
+        let nsString = CommonFeatures.composeDotSeparatedNamespace (ns)
+
+        let directives =
+            [ { LineIndentCount = 0
+                LineContent = "namespace " + nsString }
+              { LineIndentCount = 0
+                LineContent = "" }
+              { LineIndentCount = 0
+                LineContent = "module " + moduleName + "Models =" }
+              { LineIndentCount = 1
+                LineContent = "open System" } ]
+        convertIndentedLinesToString (directives, indentSpaces)
+
+    let rec getFilespacesWithExtension filespaces : string list =
+        match filespaces with
+        | hd :: [] -> [ hd + fileExtension ]
+        | hd :: tl -> hd :: getFilespacesWithExtension tl
+        | _ -> failwith "Empty list."
+
+    let transpileFilespaceDefinition (filespaceDefinition : FilespaceDefinition) =
+        let filespacesWithExtension =
+            getFilespacesWithExtension
+                (filespaceDefinition.Filespace |> Array.toList)
+        let filePath = filespacesWithExtension |> List.toArray
+        let moduleName = filespaceDefinition.Filespace |> Seq.last
+        let sourceFileCode =
+            generateSourceFileCode
+                (filespaceDefinition.Namespace, moduleName,
+                 filespaceDefinition.Models)
+        { RelativeFilePath = filePath
+          FileContent = sourceFileCode }
+
+    let transpile (models : seq<NamespaceDefinition>) =
+        let filespaceDefinitions =
+            CommonFeatures.getFilespaceDefinitions (models)
+        filespaceDefinitions
+        |> Seq.map (fun x -> transpileFilespaceDefinition x)
