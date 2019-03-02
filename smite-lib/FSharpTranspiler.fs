3a2938a2716dc7a839c0544acaa09dbddb9e2880
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-02 22:13:04 +0200

    array support in F#

--- smite-lib/FSharpTranspiler.fs
+++ smite-lib/FSharpTranspiler.fs
@@ -16,8 +16,14 @@ module FSharpTranspiler =
 
     let generateFieldCode (fieldDefinition : FieldDefinition) =
         let tn = getSpecialType (fieldDefinition.Type)
+        let atn = tn + " []"
+
+        let t =
+            match fieldDefinition.IsArray with
+            | true -> atn
+            | false -> tn
         { LineIndentCount = 2
-          LineContent = fieldDefinition.Name + ": " + tn + ";" }
+          LineContent = fieldDefinition.Name + ": " + t }
 
     let generateFieldsCode (fieldDefinitions : FieldDefinition []) =
         fieldDefinitions
