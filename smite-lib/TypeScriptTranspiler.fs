635aac055b07a48ae6ace8b7577962b4d1ae43f2
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-02 19:53:53 +0200

    basic types added for typescript

--- smite-lib/TypeScriptTranspiler.fs
+++ smite-lib/TypeScriptTranspiler.fs
@@ -1,4 +1,14 @@
 namespace TIKSN.smite.lib
 
 module TypeScriptTranspiler =
+    let fileExtension = ".ts"
+    let indentSpaces = 4
+
+    let getSpecialType (t : FieldType) =
+        match t with
+        | FieldType.BooleanType -> "boolean"
+        | FieldType.IntegerType -> "number"
+        | FieldType.RealType -> "number"
+        | FieldType.StringType -> "string"
+
     let transpile (models : seq<NamespaceDefinition>) = 0
