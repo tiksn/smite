c06f3b21b7cc5c37b802c4c82cdfaf865da26ac1
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-03-04 10:59:04 +0200

    proper usings

--- smite-lib/RoslynTranspiler.fs
+++ smite-lib/RoslynTranspiler.fs
@@ -15,15 +15,17 @@ module RoslynTranspiler =
                                 fieldType : FieldType) =
         match fieldType with
         | PrimitiveType primitiveType ->
-            syntaxGenerator.TypeExpression(getSpecialType (primitiveType))
+            (None,
+             syntaxGenerator.TypeExpression(getSpecialType (primitiveType)))
         | ComplexTypeSameNamespace typeName ->
-            syntaxGenerator.IdentifierName(typeName)
-        | ComplexTypeDifferentNamespace(_, typeName) ->
-            syntaxGenerator.IdentifierName(typeName)
+            (None, syntaxGenerator.IdentifierName(typeName))
+        | ComplexTypeDifferentNamespace(nsArray, typeName) ->
+            (Some nsArray, syntaxGenerator.IdentifierName(typeName))
 
     let generateFieldCode (syntaxGenerator : SyntaxGenerator,
                            fieldDefinition : FieldDefinition) =
-        let ts = getFieldTypeSyntaxNode (syntaxGenerator, fieldDefinition.Type)
+        let ns, ts =
+            getFieldTypeSyntaxNode (syntaxGenerator, fieldDefinition.Type)
         let ats = syntaxGenerator.ArrayTypeExpression(ts)
 
         let t =
@@ -34,7 +36,7 @@ module RoslynTranspiler =
         let fd =
             syntaxGenerator.FieldDeclaration
                 (fieldDefinition.Name, t, Accessibility.Public)
-        fd
+        (ns, fd)
 
     let generateFieldsCode (syntaxGenerator : SyntaxGenerator,
                             fieldDefinitions : FieldDefinition []) =
@@ -44,27 +46,44 @@ module RoslynTranspiler =
 
     let generateClassDeclaration (syntaxGenerator : SyntaxGenerator,
                                   model : ModelDefinition) =
-        let members = generateFieldsCode (syntaxGenerator, model.Fields)
-        syntaxGenerator.ClassDeclaration
-            (model.Name, null, Accessibility.Public, DeclarationModifiers.None,
-             null, null, members)
+        let fields = generateFieldsCode (syntaxGenerator, model.Fields)
+        let members = fields |> Seq.map (fun (_, f) -> f)
+
+        let namespaces =
+            fields
+            |> Seq.filter (fun (o, _) -> o.IsSome)
+            |> Seq.map (fun (o, _) -> o.Value)
+
+        let syntaxNode =
+            syntaxGenerator.ClassDeclaration
+                (model.Name, null, Accessibility.Public,
+                 DeclarationModifiers.None, null, null, members)
+        (namespaces, syntaxNode)
 
     let generateNamespaceDeclaration (syntaxGenerator : SyntaxGenerator,
                                       ns : string [], model : ModelDefinition) =
         let nsString = CommonFeatures.composeDotSeparatedNamespace (ns)
-        let classDefinition = generateClassDeclaration (syntaxGenerator, model)
-        syntaxGenerator.NamespaceDeclaration(nsString, classDefinition)
+        let namespaces, classDefinition =
+            generateClassDeclaration (syntaxGenerator, model)
+        let syntaxNode =
+            syntaxGenerator.NamespaceDeclaration(nsString, classDefinition)
+        (namespaces, syntaxNode)
 
     let generateSourceFileCode (syntaxGenerator : SyntaxGenerator,
                                 ns : string [], model : ModelDefinition) =
-        let usingDirectives =
-            syntaxGenerator.NamespaceImportDeclaration("System")
-        let namespaceDeclaration =
+        let namespaces, namespaceDeclaration =
             generateNamespaceDeclaration (syntaxGenerator, ns, model)
+        let namespaceDeclarations = [ namespaceDeclaration ]
+
+        let usingDirectives =
+            namespaces
+            |> Seq.map CommonFeatures.composeDotSeparatedNamespace
+            |> Seq.map (fun n -> syntaxGenerator.NamespaceImportDeclaration(n))
+            |> Seq.toList
+
+        let directives = usingDirectives @ namespaceDeclarations
         let newNode =
-            syntaxGenerator.CompilationUnit(usingDirectives,
-                                            namespaceDeclaration)
-                           .NormalizeWhitespace()
+            syntaxGenerator.CompilationUnit(directives).NormalizeWhitespace()
         newNode.ToString()
 
     let transpileModelDefinition (syntaxGenerator : SyntaxGenerator,
