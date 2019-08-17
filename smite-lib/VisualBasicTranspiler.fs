b7681834e60d33c44d56121bf1b1e1ecc2370957
Tigran TIKSN Torosyan <personal@tiksn.com>
2019-08-17 17:34:49 +0300

    Update VisualBasicTranspiler.fs

--- smite-lib/VisualBasicTranspiler.fs
+++ smite-lib/VisualBasicTranspiler.fs
@@ -9,24 +9,19 @@ module VisualBasicTranspiler =
     let fileExtension = ".vb"
     let indentSpaces = 4
 
-    let getLeadingFileComments (timeProvider : ITimeProvider) =
+    let getLeadingFileComments (timeProvider: ITimeProvider) =
         let lines =
             CommonFeatures.getFileComment (timeProvider)
             |> List.map (fun x ->
-                   { LineIndentCount = 0
-                     LineContent = "' " + x })
+                { LineIndentCount = 0
+                  LineContent = "' " + x })
         convertIndentedLinesToString (lines, indentSpaces)
 
-    let transpile (models : seq<NamespaceDefinition>,
-                   timeProvider : ITimeProvider) =
+    let transpile (models: seq<NamespaceDefinition>, fieldKind: FieldKind, timeProvider: ITimeProvider) =
         let comments = getLeadingFileComments (timeProvider)
-        let syntaxGenerator =
-            SyntaxGenerator.GetGenerator
-                (new AdhocWorkspace(), LanguageNames.VisualBasic)
-        let filespaceDefinitions =
-            CommonFeatures.getFilespaceDefinitions (models)
+        let syntaxGenerator = SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.VisualBasic)
+        let filespaceDefinitions = CommonFeatures.getFilespaceDefinitions (models)
         filespaceDefinitions
         |> Seq.collect
-               (fun x ->
-               RoslynTranspiler.transpileFilespaceDefinition
-                   (syntaxGenerator, fileExtension, x, comments))
+            (fun x ->
+            RoslynTranspiler.transpileFilespaceDefinition (syntaxGenerator, fileExtension, x, fieldKind, comments))
