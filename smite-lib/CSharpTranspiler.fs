27c4ee7aca4daec81d0e2623c7a0427e627fc7ce
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-12-06 10:38:02 +0200

    Update CSharpTranspiler.fs

--- smite-lib/CSharpTranspiler.fs
+++ smite-lib/CSharpTranspiler.fs
@@ -21,18 +21,20 @@ module CSharpTranspiler =
 
         let middleLines =
             CommonFeatures.getFileComment (timeProvider)
-            |> List.map (fun x ->
-                { LineIndentCount = 1
-                  LineContent = x })
+            |> List.map (fun x -> { LineIndentCount = 1; LineContent = x })
 
         let lines = firstLines @ middleLines @ lastLines
         convertIndentedLinesToString (lines, indentSpaces)
 
     let transpile (models: seq<NamespaceDefinition>, fieldKind: FieldKind, timeProvider: ITimeProvider) =
         let comments = getLeadingFileComments (timeProvider)
-        let syntaxGenerator = SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.CSharp)
-        let filespaceDefinitions = CommonFeatures.getFilespaceDefinitions (models)
+
+        let syntaxGenerator =
+            SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.CSharp)
+
+        let filespaceDefinitions =
+            CommonFeatures.getFilespaceDefinitions (models)
+
         filespaceDefinitions
-        |> Seq.collect
-            (fun x ->
-                RoslynTranspiler.transpileFilespaceDefinition (syntaxGenerator, fileExtension, x, fieldKind, comments))
+        |> Seq.collect (fun x ->
+            RoslynTranspiler.transpileFilespaceDefinition (syntaxGenerator, fileExtension, x, fieldKind, comments))
