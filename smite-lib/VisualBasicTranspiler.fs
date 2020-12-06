a4ad26f32328f066ba751504fcbbb1bc4039ce24
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-12-06 10:43:56 +0200

    Update VisualBasicTranspiler.fs

--- smite-lib/VisualBasicTranspiler.fs
+++ smite-lib/VisualBasicTranspiler.fs
@@ -15,13 +15,18 @@ module VisualBasicTranspiler =
             |> List.map (fun x ->
                 { LineIndentCount = 0
                   LineContent = "' " + x })
+
         convertIndentedLinesToString (lines, indentSpaces)
 
     let transpile (models: seq<NamespaceDefinition>, fieldKind: FieldKind, timeProvider: ITimeProvider) =
         let comments = getLeadingFileComments (timeProvider)
-        let syntaxGenerator = SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.VisualBasic)
-        let filespaceDefinitions = CommonFeatures.getFilespaceDefinitions (models)
+
+        let syntaxGenerator =
+            SyntaxGenerator.GetGenerator(new AdhocWorkspace(), LanguageNames.VisualBasic)
+
+        let filespaceDefinitions =
+            CommonFeatures.getFilespaceDefinitions (models)
+
         filespaceDefinitions
-        |> Seq.collect
-            (fun x ->
-                RoslynTranspiler.transpileFilespaceDefinition (syntaxGenerator, fileExtension, x, fieldKind, comments))
+        |> Seq.collect (fun x ->
+            RoslynTranspiler.transpileFilespaceDefinition (syntaxGenerator, fileExtension, x, fieldKind, comments))
