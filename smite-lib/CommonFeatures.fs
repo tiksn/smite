a8e28d02663bdf9c9cc15258d86e2921b740b53b
Tigran TIKSN Torosyan <personal@tiksn.com>
2022-12-08 13:23:37 +0100

    Update CommonFeatures.fs

--- smite-lib/CommonFeatures.fs
+++ smite-lib/CommonFeatures.fs
@@ -5,8 +5,7 @@ module CommonFeatures =
     open TIKSN.Time
 
     let getFileComment (timeProvider: ITimeProvider) =
-        let time =
-            timeProvider.GetCurrentTime().ToString("D")
+        let time = timeProvider.GetCurrentTime().ToString("D")
 
         [ "This file is auto-generated"
           "Please do not make manual changes"
@@ -17,21 +16,19 @@ module CommonFeatures =
         match fieldType with
         | PrimitiveType primitiveType -> (None, getSpecialType (primitiveType))
         | ComplexTypeSameNamespace typeName -> (None, typeName)
-        | ComplexTypeDifferentNamespace (nsArray, typeName) -> (Some nsArray, typeName)
+        | ComplexTypeDifferentNamespace(nsArray, typeName) -> (Some nsArray, typeName)
 
-    let getStartSegments (ns: string [], startingSegmentsCount) =
-        let left, _ =
-            ns |> Array.splitAt (startingSegmentsCount)
+    let getStartSegments (ns: string[], startingSegmentsCount) =
+        let left, _ = ns |> Array.splitAt (startingSegmentsCount)
 
         left
 
-    let getEndSegments (ns: string [], startingSegmentsCount) =
-        let _, right =
-            ns |> Array.splitAt (startingSegmentsCount)
+    let getEndSegments (ns: string[], startingSegmentsCount) =
+        let _, right = ns |> Array.splitAt (startingSegmentsCount)
 
         right
 
-    let hasSameStartSegments (nsSeq: seq<string []>, startingSegmentsCount) =
+    let hasSameStartSegments (nsSeq: seq<string[]>, startingSegmentsCount) =
         let nsSeqDistinct =
             (nsSeq
              |> Seq.map (fun x -> getStartSegments (x, startingSegmentsCount))
@@ -41,46 +38,36 @@ module CommonFeatures =
         nsSeqDistinct.Length = 1
 
     let getFilespaceDefinitions (namespaceDefinitions: seq<NamespaceDefinition>) =
-        let nsSeq =
-            namespaceDefinitions
-            |> Seq.map (fun x -> x.Namespace)
+        let nsSeq = namespaceDefinitions |> Seq.map (fun x -> x.Namespace)
 
         let minNamespaceLength =
-            namespaceDefinitions
-            |> Seq.map (fun x -> x.Namespace.Length)
-            |> Seq.min
+            namespaceDefinitions |> Seq.map (fun x -> x.Namespace.Length) |> Seq.min
 
         let numberOfCommonSegments =
-            seq { 0 .. minNamespaceLength }
+            seq { 0..minNamespaceLength }
             |> Seq.where (fun x -> hasSameStartSegments (nsSeq, x))
             |> Seq.max
 
         namespaceDefinitions
-        |> Seq.map
-            (fun x ->
-                { Namespace = x.Namespace
-                  Filespace =
-                      getEndSegments (x.Namespace, numberOfCommonSegments)
-                      |> Seq.toArray
-                  Models = x.Models
-                  Enumerations = x.Enumerations })
+        |> Seq.map (fun x ->
+            { Namespace = x.Namespace
+              Filespace = getEndSegments (x.Namespace, numberOfCommonSegments) |> Seq.toArray
+              Models = x.Models
+              Enumerations = x.Enumerations })
 
     let getFilespaceDefinitionsForRootOnlyNamespaces (namespaceDefinitions: seq<NamespaceDefinition>) =
         namespaceDefinitions
         |> Seq.groupBy (fun x -> x.Namespace.[0])
-        |> Seq.map
-            (fun (ns, nsds) ->
-                { Namespaces = nsds |> Seq.toArray
-                  Filespace = [| ns |] })
+        |> Seq.map (fun (ns, nsds) ->
+            { Namespaces = nsds |> Seq.toArray
+              Filespace = [| ns |] })
 
-    let composeDotSeparatedNamespace (ns: string []) = String.Join(".", ns)
+    let composeDotSeparatedNamespace (ns: string[]) = String.Join(".", ns)
 
     let rec getFilespacesWithExtension (filespaces: string list, fileExtension: string) =
         match filespaces with
         | hd :: [] -> [ hd + fileExtension ]
-        | hd :: tl ->
-            hd
-            :: getFilespacesWithExtension (tl, fileExtension)
+        | hd :: tl -> hd :: getFilespacesWithExtension (tl, fileExtension)
         | _ -> failwith "Empty list."
 
     let getFilePathWithExtension (filespaceDefinition: SingleNamespaceFilespaceDefinition, fileExtension) =
