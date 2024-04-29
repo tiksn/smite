6933ebc875a6c09912784533647a3a3e2833caf8
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-04-29 17:07:57 +0200

    Update CommonFeatures.fs

--- smite-lib/CommonFeatures.fs
+++ smite-lib/CommonFeatures.fs
@@ -2,10 +2,9 @@ namespace TIKSN.smite.lib
 
 module CommonFeatures =
     open System
-    open TIKSN.Time
 
-    let getFileComment (timeProvider: ITimeProvider) =
-        let time = timeProvider.GetCurrentTime().ToString("D")
+    let getFileComment (timeProvider: TimeProvider) =
+        let time = timeProvider.GetUtcNow().ToString("D")
 
         [ "This file is auto-generated"
           "Please do not make manual changes"
